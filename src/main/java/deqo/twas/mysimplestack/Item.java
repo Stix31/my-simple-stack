d4e6f38308bf91f9472e4c609b1fe59c3108c795
Stix31 <twashgm@gmail.com>
2019-10-24 08:18:50 +0200

    my-simple-stack-5 Test Tasks, and missing java doc.

--- src/main/java/deqo/twas/mysimplestack/Item.java
+++ src/main/java/deqo/twas/mysimplestack/Item.java
@@ -1,14 +1,25 @@
 package deqo.twas.mysimplestack;
 
-public class Item {
+/**
+ * Item.
+ */
+class Item {
 
     private int value;
 
-    public int getValue() {
+    /**
+     * Return the value of an Item.
+     * @return int.
+     */
+    int getValue() {
         return value;
     }
 
-    public void setValue(int value) {
+    /**
+     * Set value of the Item.
+     * @param value int.
+     */
+    void setValue(int value) {
         this.value = value;
     }
 }
